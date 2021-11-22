0f4e882a3d1ff5bd6029f97ec1aff23297928c59
sohamshindework <sohamshindework@gmail.com>
2021-11-22 20:27:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
+
 import ImageScroller from './MyComponents/ImageScroller';
+import Navbar from './MyComponents/Navbar';
+import Atsvideos from './MyComponents/Atsvideos';
+
 
 function App() {
   return (
     <div className="OutContainer">
+      <Navbar/>
       <ImageScroller />
+      
+      <Atsvideos/>
     </div>
   );
 }
